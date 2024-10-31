f4f49f2363fe882d0f85fd8484c8790bc0654a59
47JMC <mjabir.5b@gmail.com>
2024-10-31 21:05:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const robux_amount = document.getElementById("robuxInput").value;
+  const robux_tax_percentage = 30;
+  const robux_taxed = parseInt(robux_amount * (robux_tax_percentage / 100));
+  const robux_recieved = robux_amount - robux_taxed; // donated robux will always be higher than the robux taxed amount
+
+  const robux_dono_elem = document.getElementById("robux-dono");
+  const robux_tax_elem = document.getElementById("robux-tax");
+  const robux_got_elem = document.getElementById("robux-got");
+
+  robux_dono_elem.textContent = robux_amount;
+  robux_tax_elem.textContent = robux_taxed;
+  robux_got_elem.textContent = robux_recieved;
+}
