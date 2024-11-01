a0049d3203273d94b3b6e5ffef524c65cc68c8e1
47JMC <mjabir.5b@gmail.com>
2024-11-01 22:14:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function handleSubmit(e) {
 
   const robux_amount = document.getElementById("robuxInput").value;
   const robux_tax_percentage = 30;
-  const robux_taxed = robux_amount * (robux_tax_percentage / 100);
+  const robux_taxed = parseInt(robux_amount * (robux_tax_percentage / 100));
   const robux_recieved = robux_amount - robux_taxed; // donated robux will always be higher than the robux taxed amount
 
   const robux_dono_elem = document.getElementById("robux-dono");
