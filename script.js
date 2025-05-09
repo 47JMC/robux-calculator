76b548e5b76e025b8a304201ab7736b97ab1055a
47JMC <mjabir.5b@gmail.com>
2025-05-09 21:17:18 +0530

    fix bugs

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ function handleSubmit(e) {
 
   // Get value from param
   const searchParams = new URLSearchParams(window.location.search);
-  const robux_tax_percentage = searchParams.get("percent") || searchParams.get("p") || 30;
-
-  const robux_amount = document.getElementById("robuxInput").value;
-  const robux_taxed = parseInt(robux_amount * (robux_tax_percentage / 100));
-  const robux_recieved = robux_amount - robux_taxed; // donated robux will always be higher than the robux taxed amount
+  const robux_tax_percentage =
+    searchParams.get("percent") || searchParams.get("p") || 30;
 
   const robux_dono_elem = document.getElementById("robux-dono");
   const robux_tax_elem = document.getElementById("robux-tax");
   const robux_got_elem = document.getElementById("robux-got");
+
+  const robux_amount = document.getElementById("robuxInput").value;
+  const robux_taxed = parseInt(robux_amount * (robux_tax_percentage / 100));
+  const robux_recieved = robux_amount - robux_taxed; // donated robux will always be higher than the robux taxed amount
 
   robux_dono_elem.classList.remove("opac-animation");
   robux_tax_elem.classList.remove("opac-animation");
